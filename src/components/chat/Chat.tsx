269d252040659b89348d078429ae8af991a0ef80
artemkons <konstantinov050701@gmail.com>
2023-07-23 12:33:04 +0300

    Add chat

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -4,7 +4,17 @@ import './chat.css'
 
 const Chat = () => {
   return (
-    <div>Chat</div>
+    <div className='chat'>
+      <div className='msg-list'>
+        <div className='msg-container from-me'><span className='msg'>Сообщение</span></div>
+        <div className='msg-container'><span className='msg'>Сообщение</span></div>
+        <div className='msg-container'><span className='msg'>Сообщение</span></div>
+      </div>
+      <form>
+        <input type='text' />
+        <button>Send</button>
+      </form>
+    </div>
   )
 }
 
