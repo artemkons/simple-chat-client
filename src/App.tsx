230aed911f01ef4bf965a6cb29c4e933ff8c3117
artemkons <konstantinov050701@gmail.com>
2023-07-23 13:11:21 +0300

    Add chatMsg event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import Chat from './components/chat/Chat'
 
-import io from 'socket.io-client';
-const socket = io();
-
-socket.on('message', console.log);
-
 const App = () => {
   return (
     <Chat />
--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import io from 'socket.io-client';
 
-import './chat.css'
+import "./chat.css";
 
 const Chat = () => {
+  const [msgs, setMsgs] = useState<string[]>([])
+  const [msgTxt, setMsgTxt] = useState("");
+  const [socket, setSocket] = useState<any>(null)
+
+  useEffect(() => {
+    const socket = io();
+    setSocket(socket);
+
+    socket.on('message', (msg) => { 
+      setMsgs(msgs => [...msgs, msg]);
+    });
+  }, []);
+
+  const onMsgTxtChange = (e: any) => {
+    setMsgTxt(e.target.value);
+  }
+
+  const sendMsg = (e: any) => {
+    e.preventDefault()
+    socket.emit('chatMsg', msgTxt)
+    setMsgTxt('')
+  }
+
   return (
-    <div className='chat'>
-      <div className='msg-list'>
-        <div className='msg-container from-me'><span className='msg'>Сообщение</span></div>
-        <div className='msg-container'><span className='msg'>Сообщение</span></div>
-        <div className='msg-container'><span className='msg'>Сообщение</span></div>
+    <div className="chat">
+      <div className="msg-list">
+        {
+          msgs.map((msg: string) => 
+            <div className="msg-container from-me">
+              <span className="msg">{msg}</span>
+            </div>
+          )
+        }
       </div>
-      <form>
-        <input type='text' />
+      <form onSubmit={sendMsg}>
+        <input value={msgTxt} onChange={onMsgTxtChange} type="text" placeholder="Write a message..." />
         <button>Send</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
