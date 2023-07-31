c21919ce4100481a960a948b4493818cb36a31b9
artemkons <konstantinov050701@gmail.com>
2023-07-31 17:09:52 +0300

    Add user list

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,20 +1,34 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 
-import Chat from '../../components/Chat';
+import Chat from '../../components/Chat'
 
-import { SocketContext } from '../../context/SocketContext';
+import { SocketContext } from '../../context/SocketContext'
+
+interface User {
+  id: string
+  username: string
+  room: string
+}
 
 const Room = () => {
-  const { username, room } = useParams();
-  const socket = useContext(SocketContext);
+  const { username, room } = useParams()
+  const [roomUsers, setRoomUsers] = useState<User[]>([]);
+
+  const socket = useContext(SocketContext)
 
   useEffect(() => {
-    socket.emit('joinRoom', { username, room });
+    socket.emit('joinRoom', { username, room })
+    socket.on('enteredToRoom', ({ room, users }: { room: string, users: User[] }) => {
+      setRoomUsers(users);
+    })
   }, [])
 
   return (
     <div>
+      <ul>
+        {roomUsers.map((user) => <li>{user.username}</li>)}
+      </ul>
       {`Hey ${username}, welcome to ${room}`}
       <Chat />
     </div>
