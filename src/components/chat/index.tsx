c2e586acaa1502119bf3048dfed38f8087d8e634
artemkons <konstantinov050701@gmail.com>
2023-08-16 12:56:10 +0300

    Fix chat layout

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,43 +1,66 @@
 import React, { useContext, useEffect, useState } from "react";
 
+import { useParams } from "react-router-dom";
+
 import Message from "../Message";
+import Input from "../Input";
+import Button from "../Button";
 
 import { SocketContext } from "../../context/SocketContext";
 
 import "./chat.css";
 
+interface User {
+  id: string;
+  username: string;
+  room: string;
+}
+
+interface Message {
+  user: User;
+  msg: string;
+}
+
 const Chat = () => {
-  const [msgs, setMsgs] = useState<string[]>([])
+  const [msgs, setMsgs] = useState<Message[]>([])
   const [msgTxt, setMsgTxt] = useState("")
 
-  const socket = useContext(SocketContext)
+  const { username } = useParams();
+
+  const socket = useContext(SocketContext);
 
   useEffect(() => {
-    socket.on('message', (msg: string) => { 
-      setMsgs(msgs => [...msgs, msg]);
+    socket.on("message", ({ user, msg }: { user: User; msg: string }) => {
+      const message = { user, msg };
+      setMsgs((msgs) => [...msgs, message]);
     });
   }, []);
 
   const onMsgTxtChange = (e: any) => {
     setMsgTxt(e.target.value);
-  }
+  };
 
   const sendMsg = (e: any) => {
-    e.preventDefault()
-    socket.emit('chatMsg', msgTxt)
-    setMsgTxt('')
-  }
+    e.preventDefault();
+    socket.emit("chatMsg", msgTxt);
+    setMsgTxt("");
+  };
 
   return (
     <div className="chat">
       <div className="msg-list">
-        {
-          msgs.map((msg: string) => <Message text={msg} fromMe/>)
-        }
+        {msgs.map(({ user, msg }) => (
+          <Message text={msg} fromMe={user?.username === username} />
+        ))}
       </div>
       <form onSubmit={sendMsg}>
-        <input value={msgTxt} onChange={onMsgTxtChange} type="text" placeholder="Write a message..." />
-        <button>Send</button>
+        <Input
+          value={msgTxt}
+          onChange={onMsgTxtChange}
+          type="text"
+          placeholder="Write a message..."
+        />
+        <Button>Send</Button>
       </form>
     </div>
   );
